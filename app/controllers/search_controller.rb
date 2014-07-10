e24d8d8d7e4537438472f7bc9a2e93bb46ae3264
Lainnie <l.diallo.pro@gmail.com>
2014-07-10 10:27:18 -0400

    solr search engine

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,11 @@
 class SearchController < ApplicationController
   def index
-    
+    @request = params['search']  
+    @search = Question.search do
+      fulltext @request do
+        phrase_fields title: 2.0
+        query_phrase_slop 100
+      end
+    end
   end
 end
